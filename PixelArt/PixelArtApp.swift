86c41949662eb17892a099b61d2eb4a969a0c895
vnc003 <vnc003@Asteas-Mac-mini-3.local>
2025-02-16 20:15:42 +0200

    initial commit

---
+++ PixelArt/PixelArtApp.swift
@@ -0,0 +1,17 @@
+//
+//  PixelArtApp.swift
+//  PixelArt
+//
+//  Created by vnc003 on 16.02.25.
+//
+
+import SwiftUI
+
+@main
+struct PixelArtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
