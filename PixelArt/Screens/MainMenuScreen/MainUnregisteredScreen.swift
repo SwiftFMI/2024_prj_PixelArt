9cb5301a7fef6870f7dd1475f0b1448a4b04fdd4
Milen-Temelkov <migete@abv.bg>
2025-02-17 23:40:28 +0200

    added gitignore

---
+++ PixelArt/Screens/MainMenuScreen/MainUnregisteredScreen.swift
@@ -0,0 +1,66 @@
+import SwiftUI
+
+struct MainUnregisteredView: View {
+    var body: some View {
+        NavigationStack {
+            VStack {
+                Spacer()
+                
+                Text("Welcome to PixelArt!")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                    .padding()
+                
+                Image(systemName: "paintbrush.fill")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 100, height: 100)
+                    .foregroundColor(.blue)
+                    .padding()
+                
+                Spacer()
+                    
+                NavigationLink(destination: LoginView()) {
+                    Text("Log In")
+                        .font(.headline)
+                        .frame(maxWidth: .infinity)
+                        .padding()
+                        .background(Color.blue)
+                        .foregroundColor(.white)
+                        .cornerRadius(10)
+                        .padding(.horizontal, 40)
+                }
+                
+                NavigationLink(destination: SignUpView()) {
+                    Text("Sign Up")
+                        .font(.headline)
+                        .frame(maxWidth: .infinity)
+                        .padding()
+                        .background(Color.green)
+                        .foregroundColor(.white)
+                        .cornerRadius(10)
+                        .padding(.horizontal, 40)
+                }
+                
+                Spacer()
+            }
+        }
+    }
+}
+
+struct LoginView: View {
+    var body: some View {
+        Text("Login Screen")
+    }
+}
+
+struct SignUpView: View {
+    var body: some View {
+        Text("Sign Up Screen")
+    }
+}
+
+
+#Preview {
+    MainUnregisteredView()
+}
