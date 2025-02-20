d9176b09f50e642f584d44de2c12eef614f20007
Milen-Temelkov <migete@abv.bg>
2025-02-20 19:03:30 +0200

    feat: added method to fetch progress

--- PixelArt/ViewModels/LoggedUserViewModel.swift
+++ PixelArt/ViewModels/LoggedUserViewModel.swift
@@ -6,6 +6,7 @@ class LoggedUserViewModel: ObservableObject {
     @Published var session: FirebaseAuth.User?
     @Published var user: User?
     @Published var authError: String?
+    @Published var userPictures: [PixelPictureData] = []
     
     let db = Firestore.firestore()
     let usersCollectionName = "users"
@@ -73,6 +74,26 @@ class LoggedUserViewModel: ObservableObject {
                 try documentRef.setData(from: picture)
             } catch {
                 authError = "Failed to save picture: \(error.localizedDescription)"
+            }
+        }
+    
+    func fetchUserPictures() async {
+            guard let uid = session?.uid else {
+                authError = "User is not authenticated."
+                return
+            }
+            
+            do {
+                let snapshot = try await db.collection(usersCollectionName)
+                    .document(uid)
+                    .collection("startedPictures")
+                    .getDocuments()
+                
+                self.userPictures = try snapshot.documents.compactMap {
+                    try $0.data(as: PixelPictureData.self)
+                }
+            } catch {
+                authError = "Failed to fetch pictures: \(error.localizedDescription)"
             }
         }
     
